7c3c1c1b0bef91d250e5e3c94a07592ed887a8ac
Fulvia Mauriello <fulvivan@gmail.com>
2022-07-25 11:59:06 +0200

    Photo Like Reality

---
+++ src/logic/context.js
@@ -0,0 +1,7 @@
+const context = {
+  API_USER: "https://users-registration-api.herokuapp.com",
+  API_PHOTOS: "https://api.unsplash.com",
+  CLIENT_ID: "H4XWB85U0K6PzqCLUkPQt_2qnlGnd8C5JORrZfjXOH0",
+};
+
+export default context;
