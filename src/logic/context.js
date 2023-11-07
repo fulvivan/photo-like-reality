d5cd2c187642f5f71cbd3c657ef5ce6d5c4c2ae0
Fulvia Mauriello <fulvivan@gmail.com>
2023-11-07 14:51:58 +0100

    api user

--- src/logic/context.js
+++ src/logic/context.js
@@ -1,5 +1,5 @@
 const context = {
-  API_USER: "https://users-registration-api.herokuapp.com",
+  API_USER: "https://web-production-1842.up.railway.app",
   API_PHOTOS: "https://api.unsplash.com",
   CLIENT_ID: "H4XWB85U0K6PzqCLUkPQt_2qnlGnd8C5JORrZfjXOH0",
 };
